e694275fa3404092f8c65dffa36de1ee3615f5c6
Stacy Hanawa <stacyhanawa@gmail.com>
2019-03-03 20:50:42 -0800

    converted static site into a Django application

--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 import glob
+import os
 
 import requests
 from django.http import HttpResponse
@@ -6,11 +7,33 @@ from django.shortcuts import render
 
 templates_base = "base.html"
 
+def convert(file_path):
+    file_name = os.path.basename(file_path)
+    page_title, extension = os.path.splitext(file_name)
+    return {
+                "filename": file_path,
+                "title": page_title,
+                "output_filename": file_name
+           }
+
+def navigation():
+    all_html_files = glob.glob("content/*.html")
+    
+    pages = []
+
+    for file_path in all_html_files:
+        result = convert(file_path)
+        pages.append(result)
+    return pages
+
+pages = navigation()
 
 def index(request):
     index_html = open("content/index.html").read()
     context = {
         "content": index_html,
+        "pages": pages,
+        "selected": "index.html"
     }
     return render(request, templates_base, context)
 
@@ -18,6 +41,8 @@ def about(request):
     about_html = open("content/about.html").read()
     context = {
         "content": about_html,
+        "pages": pages,
+        "selected": "about.html"
     }
     return render(request, templates_base, context)
     
@@ -25,6 +50,8 @@ def contact(request):
     contact_html = open("content/contact.html").read()
     context = {
         "content": contact_html,
+        "pages": pages,
+        "selected": "contact.html"
     }
     return render(request, templates_base, context)
 
@@ -32,6 +59,8 @@ def post(request):
     post_html = open("content/post.html").read()
     context = {
         "content": post_html,
+        "pages": pages,
+        "selected": "post.html"
     }
     return render(request, templates_base, context)
 
