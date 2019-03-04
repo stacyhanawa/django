f0789ca0b955c638f01367bbe9f8d6103a122155
Stacy Hanawa <stacyhanawa@gmail.com>
2019-03-03 21:51:17 -0800

    created main function in manage.py and added Procfile

--- manage.py
+++ manage.py
@@ -6,18 +6,22 @@ from django.core.management import execute_from_command_line
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-settings.configure(
-    DEBUG=True,
-    ROOT_URLCONF='urls',
-    STATIC_URL='static/',
-    STATIC_ROOT=os.path.join(BASE_DIR, 'static'),
-    INSTALLED_APPS=[
-        'django.contrib.staticfiles',
-    ],
-    TEMPLATES=[{
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-    }],
-)
+def main():
+    settings.configure(
+        DEBUG=True,
+        ROOT_URLCONF='urls',
+        STATIC_URL='static/',
+        STATIC_ROOT=os.path.join(BASE_DIR, 'static'),
+        INSTALLED_APPS=[
+            'django.contrib.staticfiles',
+        ],
+        TEMPLATES=[{
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        }],
+    )
 
-execute_from_command_line(sys.argv)
+    execute_from_command_line(sys.argv)
+
+if __name__ == "__main__":
+    main()
